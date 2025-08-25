1ad857a0abbf051f635f4c8f60cdc689b9b1201a
aLearningLad <mocmanca@gmail.com>
2025-08-25 23:24:21 +0200

    additions to section one of landing

--- app/components/landing_comps/landing_nav.tsx
+++ app/components/landing_comps/landing_nav.tsx
@@ -1,9 +1,52 @@
 "use client";
 
+import Image from "next/image";
+import Link from "next/link";
+import { IoMdSearch } from "react-icons/io";
+
 const LandingNav = () => {
   return (
-    <nav className=" w-full h-20 justify-center items-center absolute bottom-0 lg:relative bg-black lg:flex ">
-      LandingNav
+    <nav className=" w-full h-20 lg:h-14 justify-between items-center absolute bottom-0 lg:relative lg:flex ">
+      {/* desktop */}
+      <div className=" w-full h-full hidden lg:flex border-4 border-black justify-center items-center ">
+        {/* logo */}
+        <div className=" w-[10%] h-full flex justify-center items-center border-4 border-white gap-1 ">
+          <Image
+            src={"/assets/applogo.png"}
+            alt="App Logo"
+            width={40}
+            height={40}
+            className=" rounded-full "
+          />
+          <p className=" italic text-[10px]">Plata.Migos</p>
+        </div>
+        {/* middle */}
+        <div className=" w-[80%] h-full flex justify-center items-center gap-8 ">
+          <Link className="nav_titles" href={"/"}>
+            Home
+          </Link>
+          <Link className="nav_titles" href={"/"}>
+            About
+          </Link>
+          <Link className="nav_titles" href={"/"}>
+            GitHub
+          </Link>
+          <Link className="nav_titles" href={"/"}>
+            Shop
+          </Link>
+        </div>
+
+        {/* search */}
+        <div className=" w-[10%] h-full flex justify-center items-center border-2 border-white gap-1">
+          <IoMdSearch size={16} />
+          <p className="nav_titles">Search</p>
+        </div>
+      </div>
+
+      {/* mobile */}
+      <div className=" flex lg:hidden w-full h-full justify-center items-center ">
+        Menu here
+      </div>
     </nav>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import LandingNav from "./components/landing_comps/landing_nav";
+import SectionOne from "./components/landing_comps/section_one";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className=" w-full h-full flex flex-col ">
       <LandingNav />
+      <SectionOne />
     </div>
   );
 }
