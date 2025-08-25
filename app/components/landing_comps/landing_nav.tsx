775f24bbd00bd09e36f02e1dfb9bcc53f35b6592
aLearningLad <mocmanca@gmail.com>
2025-08-25 20:35:21 +0200

    landing nav wireframe

---
+++ app/components/landing_comps/landing_nav.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const LandingNav = () => {
+  return (
+    <nav className=" w-full h-20 justify-center items-center absolute bottom-0 lg:relative bg-black lg:flex ">
+      LandingNav
+    </nav>
+  );
+};
+
+export default LandingNav;
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
+import LandingNav from "./components/landing_comps/landing_nav";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <Link href={"/dash"}>To Dash</Link>
+      <LandingNav />
     </div>
   );
 }
