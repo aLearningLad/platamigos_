4518289839be85a76fe5baca0c9477b9616ba0c9
aLearningLad <mocmanca@gmail.com>
2025-08-26 01:56:46 +0200

    right side ui additions

--- app/components/landing_comps/section_one/right_side.tsx
+++ app/components/landing_comps/section_one/right_side.tsx
@@ -2,8 +2,18 @@ import React from "react";
 
 const RightSide = () => {
   return (
-    <div className=" w-full lg:w-1/2 h-[40vh] lg:h-[55%] border-2 border-black ">
-      RightSide
+    <div className=" w-full lg:w-5/12 lg:pl-6 pt-8 relative h-[40vh] lg:h-[55%] border-2 border-black ">
+      {/* top block jutting in */}
+      <div className=" bg-black absolute top-0 left-[35%] rounded-lg  h-[40%] w-3/12 "></div>
+      {/* top block jutting in */}
+
+      {/* main block */}
+      <div className=" w-full lg:w-8/12 h-[80%] bg-neutral-300"></div>
+      {/* main block */}
+
+      {/* lower block jutting in */}
+      <div className=" w-3/12 h-[50%] absolute bottom-0 bg-neutral-600 left-1 rounded-lg "></div>
+      {/* lower block jutting in */}
     </div>
   );
 };
