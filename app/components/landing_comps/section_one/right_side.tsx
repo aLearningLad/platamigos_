7c2f62ba85451de33ec25b4712b4240fa29265e8
aLearningLad <mocmanca@gmail.com>
2025-08-26 02:49:39 +0200

    right side additions

--- app/components/landing_comps/section_one/right_side.tsx
+++ app/components/landing_comps/section_one/right_side.tsx
@@ -1,10 +1,29 @@
+import Image from "next/image";
 import React from "react";
 
 const RightSide = () => {
   return (
     <div className=" w-full lg:w-5/12 lg:pl-6 pt-8 items-center flex relative h-[40vh] lg:h-[55vh] border-2 border-black ">
       {/* top block jutting in */}
-      <div className=" bg-black absolute top-0 left-[23%] rounded-lg  h-[50%] w-3/12 "></div>
+      <div className=" absolute top-0 left-[23%] rounded-lg h-[50%] w-3/12 ">
+        {/* inner block */}
+        <div className=" w-full h-full relative flex items-end ">
+          <Image
+            src={"/assets/lady1.png"}
+            width={40}
+            height={15}
+            className=" absolute left-[33%] top-7 rounded-full  "
+            alt="Profile Image"
+          />
+          <div className=" bg-neutral-500/40 w-full h-[70%] rounded-lg flex flex-col justify-end items-center p-2">
+            <h3 className=" text-[10px] font-bold ">Megan Khumalo</h3>
+            <p className=" text-[8px] text-neutral-700 italic ">Lender</p>
+            <div className=" w-full bg-neutral-600/30 my-1 h-[2px] rounded-full " />
+            <span>social icons here</span>
+          </div>
+        </div>
+        {/* inner block */}
+      </div>
       {/* top block jutting in */}
 
       {/* main block */}
