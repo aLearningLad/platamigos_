acdc6aacac41b585774f2edfcbfd1a2f59b906de
aLearningLad <mocmanca@gmail.com>
2025-08-26 02:13:17 +0200

    section one progress

--- app/components/landing_comps/section_one/right_side.tsx
+++ app/components/landing_comps/section_one/right_side.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const RightSide = () => {
   return (
-    <div className=" w-full lg:w-5/12 lg:pl-6 pt-8 relative h-[40vh] lg:h-[55%] border-2 border-black ">
+    <div className=" w-full lg:w-5/12 lg:pl-6 pt-8 items-center flex relative h-[40vh] lg:h-[55vh] border-2 border-black ">
       {/* top block jutting in */}
-      <div className=" bg-black absolute top-0 left-[35%] rounded-lg  h-[40%] w-3/12 "></div>
+      <div className=" bg-black absolute top-0 left-[23%] rounded-lg  h-[50%] w-3/12 "></div>
       {/* top block jutting in */}
 
       {/* main block */}
-      <div className=" w-full lg:w-8/12 h-[80%] bg-neutral-300"></div>
+      <div className=" w-full lg:w-[55%] h-[70%] bg-neutral-300 rounded-lg "></div>
       {/* main block */}
 
       {/* lower block jutting in */}
-      <div className=" w-3/12 h-[50%] absolute bottom-0 bg-neutral-600 left-1 rounded-lg "></div>
+      <div className=" w-3/12 h-[40%] absolute bottom-0 bg-neutral-600 left-1 rounded-lg "></div>
       {/* lower block jutting in */}
     </div>
   );
--- app/components/landing_comps/section_one/section_one.tsx
+++ app/components/landing_comps/section_one/section_one.tsx
@@ -4,14 +4,19 @@ import RightSide from "./right_side";
 
 const SectionOne = () => {
   return (
-    <section className=" w-full lg:h-[calc(100vh-3.5rem)] flex flex-col lg:flex-row pt-9 lg:pt-14">
-      {/* Left Side */}
-      <LeftSide />
-      {/* Left Side */}
+    <section className=" w-full lg:h-[calc(100vh-3.5rem)] pt-9 lg:pt-14 flex flex-col">
+      <div className=" w-full h-fit flex flex-col lg:flex-row border-4 border-black">
+        {/* Left Side */}
+        <LeftSide />
+        {/* Left Side */}
 
-      {/* Right Side */}
-      <RightSide />
-      {/* Right Side */}
+        {/* Right Side */}
+        <RightSide />
+        {/* Right Side */}
+      </div>
+      <div className=" w-full h-[15vh] border-2 border-red-400 ">
+        tiles here
+      </div>
     </section>
   );
 };
