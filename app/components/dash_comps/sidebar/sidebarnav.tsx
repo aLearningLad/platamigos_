d0d7d412e6c61c815881f2f878c0c4a407c31877
aLearningLad <mocmanca@gmail.com>
2025-08-30 00:34:10 +0200

    nav tabs additions

--- app/components/dash_comps/sidebar/sidebarnav.tsx
+++ app/components/dash_comps/sidebar/sidebarnav.tsx
@@ -19,6 +19,8 @@ const SidebarNav = () => {
 
         <IoNotificationsOutline size={14} className=" text-neutral-400" />
       </div>
+
+      {/* nav tabs */}
     </div>
   );
 };
--- dev_data/sidebarinfo.tsx
+++ dev_data/sidebarinfo.tsx
@@ -1,5 +1,9 @@
 import { Tsidebar_info } from "@/models/types";
 import { IoHomeOutline } from "react-icons/io5";
+import { GiMoneyStack } from "react-icons/gi";
+import { FaHandHoldingUsd } from "react-icons/fa";
+import { RiSecurePaymentLine } from "react-icons/ri";
+import { VscGitPullRequestCreate } from "react-icons/vsc";
 
 export const sidebarinfo: Tsidebar_info[] = [
   {
@@ -7,5 +11,29 @@ export const sidebarinfo: Tsidebar_info[] = [
     icon: <IoHomeOutline />,
     title: "Dashboard",
     href: "/dash",
+  },
+  {
+    id: 8676711,
+    icon: <GiMoneyStack />,
+    title: "Fund",
+    href: "/fund",
+  },
+  {
+    id: 8305524172,
+    icon: <FaHandHoldingUsd />,
+    title: "Offers",
+    href: "/offers",
+  },
+  {
+    id: 24188271,
+    icon: <RiSecurePaymentLine />,
+    title: "Repayments",
+    href: "/repayments",
+  },
+  {
+    id: 66372615,
+    icon: <VscGitPullRequestCreate />,
+    title: "Request",
+    href: "/request",
   },
 ];
