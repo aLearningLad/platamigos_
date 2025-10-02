8dc2ffb12bc4324266f1f204fc36b40790952028
aLearningLad <mocmanca@gmail.com>
2025-10-02 12:51:53 +0200

    cta animation tweaks

--- app/components/animated_comps/main_cta_btns.tsx
+++ app/components/animated_comps/main_cta_btns.tsx
@@ -9,29 +9,39 @@ import { useRef } from "react";
 const MainCTABtns = () => {
   gsap.registerPlugin(useGSAP);
 
-  const authBtnRef = useRef<HTMLDivElement>(null);
-  const howBtnRef = useRef<HTMLDivElement>(null);
+  const authLinkRef = useRef<HTMLAnchorElement>(null);
+  const howBtnRef = useRef<HTMLButtonElement>(null);
 
   useGSAP(() => {
-    // auth
-    gsap.from(authBtnRef.current, {
+    // how to use
+    gsap.from(howBtnRef.current, {
       x: 400,
       opacity: 0,
       duration: 0.9,
     });
 
-    // how to use
+    // auth
+    gsap.from(authLinkRef.current, {
+      x: 600,
+      opacity: 0,
+      duration: 0.9,
+      delay: 0.4,
+    });
   });
 
   return (
     <span className=" w-full flex items-center justify-center lg:justify-start lg:flex-row flex-col gap-4 mt-8">
       <Link
+        ref={authLinkRef}
         href={"/auth"}
         className=" lg:h-9 h-16 w-full md:w-6/12 text-xl lg:w-4/12 flex justify-center items-center hover:bg-transparent hover:text-orange-400 hover:border-2 hover:border-orange-500 transition-all duration-300 ease-in cursor-pointer bg-orange-500 lg:text-[10px] text-white rounded-[18px] "
       >
         Get Started
       </Link>
-      <button className=" flex md:w-1/2 items-center lg:text-black text-white hover:text-white rounded-[18px] bg-cyan-600 lg:bg-transparent cursor-pointer justify-center gap-1 group h-16 lg:h-9 hover:bg-black w-full lg:w-5/12 transition-all duration-300 ease-in text-xl lg:text-[10px] ">
+      <button
+        ref={howBtnRef}
+        className=" flex md:w-1/2 items-center lg:text-black text-white hover:text-white rounded-[18px] bg-cyan-600 lg:bg-transparent cursor-pointer justify-center gap-1 group h-16 lg:h-9 hover:bg-black w-full lg:w-5/12 transition-all duration-300 ease-in text-xl lg:text-[10px] "
+      >
         <FaPlayCircle size={28} />
         How to use
       </button>
