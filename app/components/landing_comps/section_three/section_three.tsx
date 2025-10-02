62f2bb491fd66a895446461b41a295ea68ec94ef
aLearningLad <mocmanca@gmail.com>
2025-10-02 16:08:54 +0200

    section three lottie added

--- app/components/landing_comps/section_three/section_three.tsx
+++ app/components/landing_comps/section_three/section_three.tsx
@@ -11,22 +11,74 @@ import { useRef } from "react";
 const SectionThree = () => {
   gsap.registerPlugin(useGSAP);
 
+  const containerRef = useRef<HTMLDivElement | null>(null);
   const tabRef = useRef<HTMLDivElement | null>(null);
+  const titleRef = useRef<HTMLDivElement | null>(null);
+  const blurbRef = useRef<HTMLDivElement | null>(null);
+  const lottieRef = useRef<HTMLDivElement | null>(null);
+
+  useGSAP(() => {
+    // title ref
+    gsap.from(titleRef.current, {
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none restart reverse",
+      },
+      opacity: 0,
+      y: 100,
+      duration: 1.2,
+    });
+
+    // blurb
+    gsap.from(blurbRef.current, {
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none restart reverse",
+      },
+      delay: 0.8,
+      opacity: 0,
+      y: 150,
+      duration: 1.5,
+    });
+
+    // lottie
+    gsap.from(lottieRef.current, {
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "top 80%",
+        end: "top 50%",
+        toggleActions: "play none restart reverse",
+      },
+      opacity: 0,
+      scale: 0,
+      duration: 1.5,
+    });
+  });
 
   return (
-    <div className="h-[80vh] lg:pt-20 w-full py-2 lg:py-2 px-3 lg:px-72 flex flex-col justify-start items-center ">
+    <div
+      ref={containerRef}
+      className="h-[80vh] lg:pt-20 w-full py-2 lg:py-2 px-3 lg:px-72 flex flex-col justify-start items-center "
+    >
       <section className=" w-full flex lg:flex-row flex-col items-center justify-center gap-2 lg:gap-12 lg:items-end">
-        <h2 className=" text-3xl font-semibold text-center ">
+        <h2 ref={titleRef} className=" text-3xl font-semibold text-center ">
           Secure funding <br /> effortlessly
         </h2>
 
-        <p className="text-[18px] lg:text-[10px] flex w-11/12 lg:w-4/12 text-center lg:text-start ">
+        <p
+          ref={blurbRef}
+          className="text-[18px] lg:text-[10px] flex w-11/12 lg:w-4/12 text-center lg:text-start "
+        >
           Simplify loan applications and let the community decide how best to
           help you. <br /> No more hassle. <br /> No more paperwork.
         </p>
       </section>
       <section className=" w-full flex lg:justify-between lg:items-center flex-col lg:flex-row">
-        <div className=" w-full lg:w-1/2 h-full">
+        <div ref={lottieRef} className=" w-full lg:w-1/2 h-full">
           <Lottie animationData={bizLottie} className=" w-full h-full" />
         </div>
         <div className="w-full lg:w-1/2 h-full lg:h-1/2 flex flex-row lg:flex-col items-start pt-4  justify-center space-y-12">
