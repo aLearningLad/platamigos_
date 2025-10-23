ab29b8ad55f14877a89794255ce216e8914ea267
aLearningLad <mocmanca@gmail.com>
2025-10-23 22:52:11 +0200

    data export function complete

--- app/components/misc_ui/export_dynamic.tsx
+++ app/components/misc_ui/export_dynamic.tsx
@@ -20,18 +20,40 @@ const ExportDynamic = (set_is_modal: {
           .from("all_users")
           .select(
             `
-            loans(*),
-            transactions_log(*),
+            loans:loans_user_id_fkey(*),
+            transactions_log:transactions_log_debtor_id_fkey(*),
             credit_scores(*)
             `
           )
           .eq("user_id", user_id);
 
-      if (collected_data_error) throw new Error(collected_data_error.details);
+      if (collected_data_error) throw new Error(collected_data_error.message);
 
       if (collected_data) {
         toast.success("Done!");
-        console.log("Collected data here: ", collected_data);
+        set_is_loading(false);
+        set_is_modal.set_is_modal(false);
+        console.log("Collected data here: ", collected_data[0]);
+
+        //   extract my data from response
+        const raw_data = collected_data[0];
+
+        //   convert it to a string
+        const json_data = JSON.stringify(raw_data);
+
+        //   then make it a blob
+        const blob = new Blob([json_data], { type: "application/json" });
+
+        //   create my link to download from
+        const link = document.createElement("a");
+        link.href = URL.createObjectURL(blob);
+        link.download = "my_platamigos_data.json";
+
+        //   trigger the download
+        link.click();
+
+        //   tidy up
+        URL.revokeObjectURL(link.href);
       }
     } catch (error) {
       toast.error("Unable to compile your data. Please try again later");
@@ -66,7 +88,7 @@ const ExportDynamic = (set_is_modal: {
           </button>
           <button
             className=" w-full cursor-pointer sm:w-10/12 text-[12px] md:w-8/12 lg:w-6/12 h-10 lg:h-8 rounded-[8px] text-white bg-black "
-            onClick={() => set_is_modal}
+            onClick={() => set_is_modal.set_is_modal(false)}
           >
             Cancel
           </button>
