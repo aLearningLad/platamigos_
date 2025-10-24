826622f64bffeb2c65c5825d9a8b52ec1b0d89af
aLearningLad <mocmanca@gmail.com>
2025-10-24 13:22:25 +0200

    swr inside hq page

--- app/components/hq_ui/windmill_top.tsx
+++ app/components/hq_ui/windmill_top.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+"use client";
+
+import { createClient } from "@/utils/supabase/client";
+import { useState } from "react";
+import useSWR from "swr";
 
 const WindmillTop = () => {
+  const fetchDetails = async () => {
+    const supabase = createClient();
+    const user_id = (await supabase.auth.getUser()).data.user?.id;
+    try {
+      const { data: user_data, error: fetchError } = await supabase
+        .from("all_users")
+        .select("name, surname, alias")
+        .eq("user_id", user_id);
+
+      console.log("alias from db: ", user_data![0].alias);
+
+      return user_data;
+    } catch (error) {
+      console.log("Unable to fetch user's details: ", error);
+    }
+  };
+
+  const {
+    data: user_data,
+    error: user_data_error,
+    isLoading,
+  } = useSWR("user-details", fetchDetails);
+
   return (
     <span className=" w-full items-center flex h-[15%] justify-between py-3 lg:py-0 ">
       <p className=" text-[18px] lg:text-[12px] italic text-neutral-700">
-        thathooo_
+        {user_data![0].alias}
       </p>
       <div className=" w-fit px-2 h-fit bg-neutral-700/10 rounded-[12px] ">
         <select
