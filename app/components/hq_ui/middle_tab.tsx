6e544ca65c890fb202e1efa036250895e32ccb62
aLearningLad <mocmanca@gmail.com>
2025-10-24 14:33:42 +0200

    middle tab animations

--- app/components/hq_ui/middle_tab.tsx
+++ app/components/hq_ui/middle_tab.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { I_middle_tab } from "@/models/interfaces";
+import { useGSAP } from "@gsap/react";
 import Lottie from "lottie-react";
+import { useRef } from "react";
+import { gsap } from "gsap";
 
 const MiddleTab: React.FC<I_middle_tab> = ({
   animation,
@@ -9,10 +12,25 @@ const MiddleTab: React.FC<I_middle_tab> = ({
   tab_text,
   tab_value,
 }) => {
+  const tab3Ref = useRef<HTMLDivElement | null>(null);
+  const tab4Ref = useRef<HTMLDivElement | null>(null);
+
+  gsap.registerPlugin(useGSAP);
+
+  useGSAP(() => {
+    // tab3
+    gsap.from(tab3Ref.current, {
+      scale: 0.3,
+      duration: 1.2,
+      ease: "bounce.out",
+    });
+  });
+
   return (
     <div
       key={id}
       className=" w-full lg:w-1/2 h-fit lg:h-full p-4 bg-neutral-600/10 lg:bg-white flex flex-col items-center justify-center rounded-2xl "
+      ref={tab_text === "Loans funded so far" ? tab3Ref : tab4Ref}
     >
       <Lottie
         animationData={animation}
