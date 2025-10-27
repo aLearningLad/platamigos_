02d4506afecc018d64e3800b0a59d80a9babe64e
aLearningLad <mocmanca@gmail.com>
2025-10-27 22:19:23 +0200

    potential fix for fonts ignored in prod

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
