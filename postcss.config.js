c94a116a1dcc17088acb98bae44cb51822ae78c1
aLearningLad <mocmanca@gmail.com>
2025-10-28 22:45:49 +0200

    Fix PostCSS config format for Vercel

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,9 @@
+// module.exports = {
+//   plugins: ["@tailwindcss/postcss"],
+// };
+
 module.exports = {
-  plugins: ["@tailwindcss/postcss"],
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
